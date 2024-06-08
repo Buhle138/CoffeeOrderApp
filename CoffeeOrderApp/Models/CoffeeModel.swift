cdf2460ef0acb7bbf5a1a4321bd8b521098c1d5c
buhleradzilani <radzilanibuhle@gmail.com>
2024-06-08 11:59:59 +0200

    coffee model complete with the webservice. we get all the orders from webservice getOrders() method.

--- CoffeeOrderApp/Models/CoffeeModel.swift
+++ CoffeeOrderApp/Models/CoffeeModel.swift
@@ -7,7 +7,19 @@
 
 import Foundation
 
-
+@MainActor //sinced the orders are marked as published we want to execute them on the main thread
 class CoffeeModel: ObservableObject{
+    
+    let webservice: Webservice
+    @Published private(set) var orders: [Order] = []
+    
+    
+    init(webservice: Webservice){
+        self.webservice = webservice
+    }
+    
+    func populateOrders() async throws {
+      orders =  try await webservice.getOrders()
+    }
     
 }
