a5b3e4a4a4b84f69bd5245f24a5f2247a2879d1c
buhleradzilani <radzilanibuhle@gmail.com>
2024-06-08 10:22:25 +0200

    Initial Commit

---
+++ CoffeeOrderApp/CoffeeOrderAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeOrderAppApp.swift
+//  CoffeeOrderApp
+//
+//  Created by Buhle Radzilani on 2024/06/08.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeOrderAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
